131375317249d1252cc7a3c60007c4e86007d9dd
arvicrin <1231@holbertonschool.com>
2020-09-30 23:24:10 +0000

    Another way to verify if a BTree is a RED-BLACK Tree

--- 0x00-red_black_tree/1-rb_tree_is_valid.c
+++ 0x00-red_black_tree/1-rb_tree_is_valid.c
@@ -2,17 +2,25 @@
 
 /**
 * is_BTS_and_colors - verify is the tree is BTS, if each node has a color
-* and if there is not exist 2 consecutive red nodes
+* , if there is not exist 2 consecutive red nodes, and if all path has the
+* same nums of black nodes
 * @tree: root of the tree
 * @prev_color: previous node color
+* @num_black: number of black nodes of the tree
+* @num_black_path: number of black nodes that each path has
 * Return: 1 on success, 0 otherwise
 */
-int is_BTS_and_colors(rb_tree_t *tree, int prev_color)
+int is_BTS_and_colors(rb_tree_t *tree, int prev_color, int num_black,
+	int num_black_path)
 {
 	int left = 0, right = 0;
 
 	if (!tree)
-		return (1);
+	{
+		if (num_black == num_black_path + 1)
+			return (1);
+		return (0);
+	}
 
 	if ((tree->color != BLACK && tree->color != RED) ||
 		(prev_color == 1 && tree->color == RED))
@@ -22,57 +30,30 @@ int is_BTS_and_colors(rb_tree_t *tree, int prev_color)
 		(tree->right != NULL && tree->n > tree->right->n))
 		return (0);
 
-	left = is_BTS_and_colors(tree->left, (tree->color == RED) ? 1 : 0);
-	if (!left)
-		return (0);
-
-	right = is_BTS_and_colors(tree->right, (tree->color == RED) ? 1 : 0);
-	if (!right)
-		return (0);
-
-	return (1);
-}
-
-/**
-* same_black_node - verify if all path in tree has the same black nodes
-* @tree: root node of the tree
-* @num_black: number of black nodes of the tree
-* @num_black_path: number of black nodes that each path has
-* Return: 1 on success, 0 otherwise
-*/
-int same_black_node(rb_tree_t *tree, int num_black, int num_black_path)
-{
-	int same_black = 0;
-
-	if (!tree)
-	{
-		if (num_black == num_black_path + 1)
-			return (1);
-		return (0);
-	}
-
 	if (tree->color == BLACK)
 	{
-		same_black = same_black_node(tree->left, num_black, num_black_path + 1);
-		if (!same_black)
+		left = is_BTS_and_colors(tree->left, (tree->color == RED) ? 1 : 0,
+			num_black, num_black_path + 1);
+		if (!left)
 			return (0);
-		same_black = same_black_node(tree->right, num_black, num_black_path + 1);
-		if (!same_black)
+		right = is_BTS_and_colors(tree->right, (tree->color == RED) ? 1 : 0,
+			num_black, num_black_path + 1);
+		if (!right)
 			return (0);
 	}
 	else
 	{
-		same_black = same_black_node(tree->left, num_black, num_black_path);
-		if (!same_black)
+		left = is_BTS_and_colors(tree->left, (tree->color == RED) ? 1 : 0,
+			num_black, num_black_path);
+		if (!left)
 			return (0);
-		same_black = same_black_node(tree->right, num_black, num_black_path);
-		if (!same_black)
+		right = is_BTS_and_colors(tree->right, (tree->color == RED) ? 1 : 0,
+			num_black, num_black_path);
+		if (!right)
 			return (0);
 	}
-
 	return (1);
 }
-
 
 /**
 * rb_tree_is_valid - verify if a given tree is a red-black tree
@@ -87,9 +68,6 @@ int rb_tree_is_valid(const rb_tree_t *tree)
 	if (!tree || tree->color == RED)
 		return (0);
 
-	if (!is_BTS_and_colors((rb_tree_t *) tree, 2))
-		return (0);
-
 	while (tree_copy)
 	{
 		if (tree_copy->color == BLACK)
@@ -98,7 +76,7 @@ int rb_tree_is_valid(const rb_tree_t *tree)
 		if (!tree_copy)
 			num_black++;
 	}
-	if (!same_black_node((rb_tree_t *) tree, num_black, 0))
+	if (!is_BTS_and_colors((rb_tree_t *) tree, 2, num_black, 0))
 		return (0);
 	return (1);
 }
--- 0x00-red_black_tree/rb_trees.h
+++ 0x00-red_black_tree/rb_trees.h
@@ -3,6 +3,17 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+
+#define GETPARENT(node) ((node == NULL) ? NULL : node->parent)
+#define GETGRANDPARENT(node) GETPARENT(GETPARENT(node))
+#define GETSIBLING(node) \
+	(GETPARENT(node) ? \
+	((node == (GETPARENT(node)->left)) ? \
+	(GETPARENT(node)->right) : \
+	(GETPARENT(node)->left)) : \
+	NULL)
+#define GETUNCLE(node) GETSIBLING(GETPARENT(node))
+
 /**
  * enum rb_color_e - Possible color of a Red-Black tree
  *
@@ -37,5 +48,6 @@ typedef struct rb_tree_s
 
 rb_tree_t *rb_tree_node(rb_tree_t *parent, int value, rb_color_t color);
 int rb_tree_is_valid(const rb_tree_t *tree);
+rb_tree_t *rb_tree_insert(rb_tree_t **tree, int value);
 
 #endif /* _RB_TREES_H_ */
