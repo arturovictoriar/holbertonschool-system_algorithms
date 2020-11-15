190ddb3d8693a3c805ca2d65436c41b4ecb07342
arvicrin <1231@holbertonschool.com>
2020-11-15 20:55:06 +0000

    break when action be NULL

--- 0x01-graphs/5-breadth_first_traverse.c
+++ 0x01-graphs/5-breadth_first_traverse.c
@@ -94,7 +94,7 @@ size_t breadth_first_traverse(const graph_t *graph,
 	char len_gra[2048] = {0};
 	size_t depth_n = 0, max_depth = 0;
 
-	if (!graph)
+	if (!graph || !action)
 		return (0);
 
 	cp_ve_header = graph->vertices;
