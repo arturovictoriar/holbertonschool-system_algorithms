cd53aab93c63f7de7a0ca3697e37fbf738813279
arvicrin <1231@holbertonschool.com>
2020-12-15 20:20:42 +0000

    Refactorize heap_extract.c

--- 0x02-huffman_coding/heap/heap_extract.c
+++ 0x02-huffman_coding/heap/heap_extract.c
@@ -1,14 +1,14 @@
 #include "heap.h"
 
 /**
- * heapify_down - fix the heap_min
+ * heapify - fix the heap_min
  * @heap: heap struct
  * Return: Nothing
  */
-void heapify_down(heap_t *heap)
+void heapify(heap_t *heap)
 {
-	binary_tree_node_t *node = heap->root, *child;
-	void *temp;
+	binary_tree_node_t *node = heap->root, *child = NULL;
+	void *temp = NULL;
 
 	while (1)
 	{
@@ -17,15 +17,43 @@ void heapify_down(heap_t *heap)
 		if (!node->right)
 			child = node->left;
 		else
-			child = heap->data_cmp(node->left->data, node->right->data) <= 0 ?
-				node->left : node->right;
+		{
+			if (heap->data_cmp(node->left->data, node->right->data) <= 0)
+				child = node->left;
+			else
+				child = node->right;
+		}
 		if (heap->data_cmp(node->data, child->data) < 0)
 			break;
-		temp = node->data;
-		node->data = child->data;
-		child->data = temp;
+		temp = node->data, node->data = child->data, child->data = temp;
 		node = child;
 	}
+}
+
+/**
+ * get_last_node - get the last node in heap_min
+ * @heap: heap struct
+ * Return: last node in heap_min
+ */
+binary_tree_node_t *get_last_node(heap_t *heap)
+{
+	binary_tree_node_t *node = NULL;
+	size_t node_level = 0;
+
+	for (node_level = 1; node_level <= heap->size; node_level <<= 1)
+		;
+	node_level >>= 2;
+
+
+	for (node = heap->root; node_level > 0; node_level >>= 1)
+	{
+		if (heap->size & node_level)
+			node = node->right;
+		else
+			node = node->left;
+	}
+
+	return (node);
 }
 
 /**
@@ -35,32 +63,24 @@ void heapify_down(heap_t *heap)
  */
 void *heap_extract(heap_t *heap)
 {
-	binary_tree_node_t *node;
-	void *data;
-	size_t bit;
+	binary_tree_node_t *node = NULL;
+	void *data = NULL;
 
 	if (!heap || !heap->root)
 		return (NULL);
 
 	data = heap->root->data;
 	if (heap->size == 1)
-	{
-		free(heap->root);
-		heap->root = NULL, heap->size--;
-		return (data);
-	}
-	for (bit = 1; bit <= heap->size; bit <<= 1)
-		;
-	bit >>= 2;
-	for (node = heap->root; bit > 0; bit >>= 1)
-		node = heap->size & bit ? node->right : node->left;
+		return (free(heap->root), heap->root = NULL, --heap->size, data);
+
+	node = get_last_node(heap);
+
 	heap->root->data = node->data;
 	if (node->parent->left == node)
 		node->parent->left = NULL;
 	else
 		node->parent->right = NULL;
-	heap->size--;
-	free(node);
-	heapify_down(heap);
+
+	--heap->size, free(node), heapify(heap);
 	return (data);
 }
